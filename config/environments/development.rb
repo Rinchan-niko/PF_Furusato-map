1f968c5224a08eb610dfa9aeaf59984d6b24a407
Rinchan-niko <rinchan.niko@gmail.com>
2022-07-13 09:28:15 +0000

    モデル

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  config.hosts << "36683d8f79ad4243b6cc23a02b39c9ce.vfs.cloud9.ap-northeast-1.amazonaws.com"
+
 end
