35b9f6921149133b542395ed74cfe5a51e377c31
Rinchan-niko <rinchan.niko@gmail.com>
2022-08-01 02:44:04 +0000

    break

--- app/controllers/admin/comments_controller.rb
+++ app/controllers/admin/comments_controller.rb
@@ -1,7 +1,15 @@
 class Admin::CommentsController < ApplicationController
   def index
+    @comments = Comment.page(params[:page]).per(10)
   end
 
   def show
+    @comment = Comment.find(params[:id])
+  end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    redirect_to admin_comments_path
   end
 end
--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -1,7 +1,15 @@
 class Admin::PostsController < ApplicationController
   def index
+    @posts = Post.page(params[:page]).per(10)
   end
 
   def show
+    @post = Post.find(params[:id])
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to admin_posts_path
   end
 end
